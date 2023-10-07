2b68a592f9ce03f3073c44082533e72d786edebb
Rizal Anditama <rizalanditama2105@gmail.com>
2023-10-07 12:13:25 +0700

    basic authentication

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->add('login', 'UserController::login', ['filter' => 'noAuth']);
+$routes->add('register', 'UserController::register', ['filter' => 'noAuth']);
+
+$routes->post('logout', 'UserController::logout', ['filter' => 'noAuth']);
