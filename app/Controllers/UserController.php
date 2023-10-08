960e70f2efd378b5edebeb6affa5c8bb0e562988
Rizal Anditama <rizalanditama2105@gmail.com>
2023-10-08 10:06:18 +0700

    changing the `userController` into having less if

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -11,35 +11,35 @@ class UserController extends BaseController
     {
         $data = [];
 
-        if ($this->request->getMethod() == 'post') {
-
-            $rules = [
-                'username' => 'required',
-                'password' => 'required|min_length[8]|max_length[255]|validateUser[username,password]',
-            ];
-
-            $errors = [
-                'password' => [
-                    'validateUser' => "Username or Password didn't match",
-                ],
-            ];
-
-            if (!$this->validate($rules, $errors)) {
-
-                return view('login', [
-                    "validation" => $this->validator,
-                ]);
-            } else {
-                $model = new UserModel();
-
-                $user = $model->where('username', $this->request->getVar('username'))
-                    ->first();
-
-                // Storing session values
-                $this->setUserSession($user);
-            }
+        if ($this->request->getMethod() != 'post') {
+            return view('login', $data);
         }
-        return view('login');
+
+        $rules = [
+            'username' => 'required',
+            'password' => 'required|min_length[8]|max_length[255]|validateUser[username,password]',
+        ];
+
+        $errors = [
+            'password' => [
+                'validateUser' => "Username or Password didn't match",
+            ],
+        ];
+
+        if (!$this->validate($rules, $errors)) {
+
+            return view('login', [
+                "validation" => $this->validator,
+            ]);
+        }
+
+        $model = new UserModel();
+
+        $user = $model->where('username', $this->request->getVar('username'))
+            ->first();
+
+        // Storing session values
+        $this->setUserSession($user);
     }
 
     private function setUserSession($user)
@@ -59,32 +59,33 @@ class UserController extends BaseController
     {
         $data = [];
 
-        if ($this->request->getMethod() == 'post') {
-            $rules = [
-                'username' => 'required|min_length[3]|max_length[20]|is_unique[user.username]',
-                'password' => 'required|min_length[8]|max_length[255]',
-                'password_confirm' => 'matches[password]',
-            ];
-
-            if (!$this->validate($rules)) {
-                return view('register', [
-                    "validation" => $this->validator,
-                ]);
-            } else {
-                $model = new UserModel();
-
-                $newData = [
-                    'username' => $this->request->getVar('username'),
-                    'email' => $this->request->getVar('email'),
-                    'password' => $this->request->getVar('password'),
-                ];
-                $model->save($newData);
-                $session = session();
-                $session->setFlashdata('success', 'Successful Registration');
-                return redirect()->to(base_url('login'));
-            }
+        if ($this->request->getMethod() != 'post') {
+            return view('register', $data);
         }
-        return view('register');
+
+        $rules = [
+            'username' => 'required|min_length[3]|max_length[20]|is_unique[user.username]',
+            'password' => 'required|min_length[8]|max_length[255]',
+            'password_confirm' => 'matches[password]',
+        ];
+
+        if (!$this->validate($rules)) {
+            return view('register', [
+                "validation" => $this->validator,
+            ]);
+        }
+
+        $model = new UserModel();
+
+        $newData = [
+            'username' => $this->request->getVar('username'),
+            'email' => $this->request->getVar('email'),
+            'password' => $this->request->getVar('password'),
+        ];
+        $model->save($newData);
+        $session = session();
+        $session->setFlashdata('success', 'Successful Registration');
+        return redirect()->to(base_url('login'));
     }
 
     public function logout()
